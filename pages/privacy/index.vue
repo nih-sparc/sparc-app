d44998bd8c2e35dd47ba53eed30c9203fb089736
Cameron Baney <cbaney@gmail.com>
2020-01-31 11:38:21 -0500

    Terms and privacy plicy

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="privacy-policy">
-    Privacy Policy
+    <div class="page-wrap container">
+      <div class="subpage">
+        <el-row type="flex" justify="center">
+          <el-col :row="24">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+              similique numquam accusamus quae, aliquid vero obcaecati facilis
+              reprehenderit. Fugit quibusdam placeat repellendus vitae similique
+              reprehenderit, aspernatur velit blanditiis non dicta.
+            </p>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,8 +23,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.privacy-policy {
-  margin-top: 9rem;
-}
-</style>
+<style lang="scss" scoped></style>
