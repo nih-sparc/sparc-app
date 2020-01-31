31af6bdbe83493aaceca86c2a2c7380c47c057a8
Cameron Baney <cbaney@gmail.com>
2020-01-31 11:11:34 -0500

    Resources page

--- pages/resources/index.vue
+++ pages/resources/index.vue
@@ -1,17 +1,40 @@
 <template>
-<div class="resources">
-    Resources
-</div>
+  <div class="resources">
+    <page-hero v-if="heroCopy" class="subpage">
+      <p>
+        {{ heroCopy }}
+      </p>
+    </page-hero>
+    <div class="page-wrap container">
+      <div class="subpage">
+        <el-row type="flex" justify="center">
+          <el-col :row="24" />
+        </el-row>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import PageHero from '@/components/PageHero/PageHero.vue'
+
 export default {
-  name: 'Resources'
+  name: 'Resources',
+
+  components: {
+    PageHero
+  },
+
+  data() {
+    return {
+      heroCopy: ''
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
- .resources {
-   margin-top: 9rem;
- }
+.resources {
+  margin-top: 9rem;
+}
 </style>
