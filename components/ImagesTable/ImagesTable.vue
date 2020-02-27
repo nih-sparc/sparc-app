f08a96e16d79760f961485ea701838a9983b68c8
Cameron Baney <cbaney@gmail.com>
2020-02-27 13:54:03 -0500

    Functionality for image viewer route

--- components/ImagesTable/ImagesTable.vue
+++ components/ImagesTable/ImagesTable.vue
@@ -1,20 +1,22 @@
 <template>
   <el-table :data="tableData" empty-text="No Results">
-    <el-table-column :fixed="true" prop="image_id" label="ID" width="300">
+    <el-table-column :fixed="true" prop="image_id" label="ID">
       <template slot-scope="scope">
-        <!-- <nuxt-link
+        <nuxt-link
           :to="{
-            name: 'file-datasetId-datasetVersion-path,
+            name: 'datasets-viewer-id',
             params: {
-              datasetId
-             }
+              id: scope.row.image_id
+            },
+            query: {
+              viewer: viewerId(scope.row.share_link)
+            }
           }"
-        > -->
-        {{ scope.row.image_id }}
-        <!-- </nuxt-link> -->
+        >
+          {{ scope.row.image_id }}
+        </nuxt-link>
       </template>
     </el-table-column>
-    <el-table-column prop="share_link" label="Link" />
   </el-table>
 </template>
 
@@ -29,7 +31,18 @@ export default {
     }
   },
 
-  methods: {}
+  methods: {
+    /**
+     * Get the viewer ID from the share link
+     * @param {String} shareLink
+     * @returns {String}
+     */
+    viewerId: function(shareLink) {
+      const linkParts = shareLink.split(process.env.BL_SHARE_LINK_PREFIX)
+
+      return linkParts[1]
+    }
+  }
 }
 </script>
 
