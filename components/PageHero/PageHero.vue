e85376da6a78f3dcb6284e1397f5c240f79f5754
Cameron Baney <cbaney@gmail.com>
2020-02-28 14:44:34 -0500

    Hero styles

--- components/PageHero/PageHero.vue
+++ components/PageHero/PageHero.vue
@@ -2,11 +2,11 @@
   <div class="page-hero">
     <div class="container">
       <div class="row">
-        <div class="col">
+        <div class="col page-hero__copy">
           <slot />
         </div>
-        <div class="col">
-          <slot name="pagehero image" />
+        <div v-if="$slots['image']" class="page-hero__image">
+          <slot name="image" />
         </div>
       </div>
     </div>
@@ -32,15 +32,46 @@ export default {
   font-size: 1em;
   line-height: 1.5rem;
   padding: 2rem 1rem;
-  @media (min-width: 768px) {
+  @media (min-width: 48em) {
     font-size: 1.25em;
     line-height: 2rem;
     padding: 2.5rem 0;
+  }
+  &__copy {
+    position: relative;
+    z-index: 1;
+    @media (min-width: 64em) {
+      max-width: 53rem;
+    }
+  }
+  &__image {
+    display: none;
+
+    @media (min-width: 48em) {
+      display: block;
+      height: calc(100% + 5rem);
+      right: 0;
+      position: absolute;
+      top: -2.5rem;
+      width: auto;
+    }
+    img {
+      display: block;
+      height: 100%;
+      width: inherit;
+    }
+  }
+  .container {
+    position: relative;
   }
   h1 {
     font-size: 1.75em;
     margin-bottom: 0.5rem;
     line-height: 1.2;
+  }
+  h1,
+  p {
+    background-color: rgba(36, 36, 91, 0.75);
   }
   p:last-child {
     margin: 0;
@@ -48,13 +79,6 @@ export default {
 
   .row {
     display: flex;
-  }
-
-  .page-hero-img {
-    height: 19rem;
-    @media screen and (max-width: 767px) {
-      display: none;
-    }
   }
 }
 </style>
