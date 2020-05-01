f61e702d9495e536d3ce53a414b91014a2960d94
Riccardo-Maio <riccardo.maio@gmail.com>
2020-05-01 10:47:30 -0400

    removed commented code

--- pages/news-and-events/model.ts
+++ pages/news-and-events/model.ts
@@ -94,7 +94,6 @@ export interface Data {
 
 export interface Computed {
   displayedUpcomingEvents: EventsEntry[],
-  // pastEventsReversed: EventsEntry[],
   pastEventsChunkSize: number,
   displayedPastEvents: EventsEntry[]
 }
