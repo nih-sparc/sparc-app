1c3bdf236a1b44da48bc399931f1f8f2c31ad8ec
alan-wu <alan.wu@auckland.ac.nz>
2020-05-08 10:32:23 +1200

    Make app always in the middle.

--- pages/map/index.vue
+++ pages/map/index.vue
@@ -5,12 +5,12 @@
       <h1>Map</h1>
       <div class="sideText">
         Interactive maps reveal the anatomy and functional
-          relationships of the autonomic nerves and the organs that they
-          innervate. 2D and 3D maps render spatial dynamics, connectivity, and
-          physiology across a range of species and nerve-organ systems.
+        relationships of the autonomic nerves and the organs that they
+        innervate. 2D and 3D maps render spatial dynamics, connectivity, and
+        physiology across a range of species and nerve-organ systems.
       </div>
     </page-hero>
-    <div class="page-wrap portalmapcontainer">
+    <div class="page-wrap portalmapcontainer" ref="mappage">
       <client-only placeholder="Loading...">
         <div class="map-app">
           <MapContent />
@@ -21,76 +21,65 @@
 </template>
 
 <script>
-import Breadcrumb from '@/components/Breadcrumb/Breadcrumb.vue'
-import PageHero from '@/components/PageHero/PageHero.vue'
-process.client ? import('@abi-software/mapintegratedvuer/dist/mapintegratedvuer.css') : null
+import Breadcrumb from "@/components/Breadcrumb/Breadcrumb.vue";
+import PageHero from "@/components/PageHero/PageHero.vue";
+process.client
+  ? import("@abi-software/mapintegratedvuer/dist/mapintegratedvuer.css")
+  : null;
 
 export default {
-  name: 'Map',
+  name: "Map",
 
   components: {
     Breadcrumb,
     PageHero,
-    MapContent: process.client ? () => import('@/components/MapApp/MapApp') : null
+    MapContent: process.client
+      ? () => import("@/components/MapApp/MapApp")
+      : null
   },
 
   data() {
     return {
       resources: [],
-      title: 'Map',
+      title: "Map",
       breadcrumb: [
         {
           to: {
-            name: 'index'
+            name: "index"
           },
-          label: 'Home'
+          label: "Home"
         }
       ]
-    }
+    };
   },
 
-
-  methods: {
- 
-
-  }
-}
+  methods: {}
+};
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/_variables.scss';
+@import "@/assets/_variables.scss";
 .map {
-
-  .sideText{
+  .sideText {
     top: 0px;
-    font-size:0.8em;
-    position:absolute;
-    line-height:1.2em;
-    width:40em;
-    margin-left:6em; 
+    font-size: 0.8em;
+    position: absolute;
+    line-height: 1.2em;
+    width: 40em;
+    margin-left: 6em;
   }
 
   .portalmapcontainer {
     margin-top: 1.5rem;
-    height:90vh;
-    max-width:none;
-
-    @media screen and (min-width: 1440px) {
-      width: 1393px;
-      height: 778px;
-      padding-left:24px;
-    }
-
-    @media screen and (min-width: 1920px) {
-      width: 1851px ;
-      height: 938px ;
-      padding-left:34px;
-    }
+    height: 90vh;
+    max-width: calc(100% - 48px);
+    padding-left:24px;
   }
+
   .map-app {
-    position:relative;
-    width:100%;
-    height:100%;
+    position: relative;
+    width: 100%;
+    height: 100%;
     border: solid 1px #dcdfe6;
     box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.06);
   }
@@ -112,6 +101,5 @@ export default {
       padding-top: 0;
     }
   }
-
 }
 </style>
