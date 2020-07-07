77f04bc0ccf24467d9a251a2b0492dca06e6bbb8
Cameron Baney <cbaney@gmail.com>
2020-07-07 16:42:04 -0400

    Make all markdown links open in a new tab

--- mixins/marked/index.js
+++ mixins/marked/index.js
@@ -1,7 +1,19 @@
 import marked from 'marked'
 
+/**
+ * Modify the link renderer to add `target="blank"`
+ * https://github.com/markedjs/marked/pull/1371#issuecomment-434320596
+ */
+const renderer = new marked.Renderer()
+
+renderer.link = function(href, title, text) {
+  const link = marked.Renderer.prototype.link.apply(this, arguments)
+  return link.replace('<a', "<a target='_blank'")
+}
+
 marked.setOptions({
-  sanitize: true
+  sanitize: true,
+  renderer
 })
 
 export default {
