e7b600cbc38a181d8b652778b93555e2dc634c2e
alan-wu <alan.wu@auckland.ac.nz>
2020-12-17 10:57:19 +1300

    Add the ability to pass in flatmapapi endpoint.

--- pages/maps/index.vue
+++ pages/maps/index.vue
@@ -14,7 +14,7 @@
     <div class="page-wrap portalmapcontainer" ref="mappage">
       <client-only placeholder="Loading...">
         <div class="mapClass">
-          <MapContent ref="map" :state="state" :api="api" :shareLink="shareLink" @updateShareLinkRequested="updateUUID" />
+          <MapContent ref="map" :state="state" :api="api" :flatmapapi="flatmapapi" :shareLink="shareLink" @updateShareLinkRequested="updateUUID" />
         </div>
       </client-only>
     </div>
@@ -50,6 +50,7 @@ export default {
       prefix: "/maps",
       state: undefined,
       api: process.env.portal_api,
+      flatmapapi: process.env.flatmap_api,
     };
   },
   computed: {
