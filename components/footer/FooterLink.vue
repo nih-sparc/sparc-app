4062eb070d4807b954d080a22f7d1617bb34019b
Cameron Baney <cbaney@gmail.com>
2021-02-25 12:08:24 -0500

    Add policies and new layout for Footer

---
+++ components/footer/FooterLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <nuxt-link v-if="isInternalLink(link.fields.url || '')" :to="link.fields.url">
+    {{ link.fields.title }}
+  </nuxt-link>
+  <a v-else :href="link.fields.longUrl || link.fields.url" target="_blank">
+    {{ link.fields.title }}
+  </a>
+</template>
+
+<script>
+import { isInternalLink } from '@/mixins/marked/index'
+
+export default {
+  name: 'FooterLink',
+
+  props: {
+    link: {
+      type: Object,
+      default: () => {}
+    }
+  },
+
+  methods: {
+    isInternalLink
+  }
+}
+</script>
