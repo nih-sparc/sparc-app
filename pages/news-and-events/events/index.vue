56653c1f4b4fe14d0d99bdd08b02fc4cf5ea35d5
Cameron Baney <cbaney@gmail.com>
2021-03-29 11:24:13 -0400

    feat(Events): Events landing page

---
+++ pages/news-and-events/events/index.vue
@@ -0,0 +1,273 @@
+<template>
+  <div class="events-page">
+    <breadcrumb :breadcrumb="breadcrumb" :title="title" />
+    <page-hero>
+      <!-- <h1>{{ heroData.fields.page_title }}</h1>
+      <p>
+        {{ heroData.fields.heroCopy }}
+      </p>
+      <search-controls-contentful
+        placeholder="Search news and events"
+        path="/news-and-events"
+      />
+      <img
+        v-if="heroData.fields.heroImage"
+        slot="image"
+        class="page-hero-img"
+        :src="heroData.fields.heroImage.fields.file.url"
+      /> -->
+    </page-hero>
+    <div class="page-wrap">
+      <div class="container">
+        <h2>Events</h2>
+        <tab-nav
+          :tabs="eventsTabs"
+          :active-tab="activeTab"
+          @set-active-tab="activeTab = $event"
+        />
+        <div class="events-wrap">
+          <template v-if="activeTab === 'upcoming'">
+            <div class="upcoming-events">
+              <event-card
+                v-for="event in displayedUpcomingEvents"
+                :key="event.sys.id"
+                :event="event"
+              />
+            </div>
+
+            <div class="show-all-upcoming-events">
+              <a
+                v-if="!isShowingAllUpcomingEvents"
+                class="show-all-upcoming-events__btn"
+                href="#"
+                @click.prevent="isShowingAllUpcomingEvents = true"
+              >
+                Show All ({{ upcomingEvents.length }}) Upcoming Events
+                <svg-icon name="icon-sort-desc" height="10" width="10" />
+              </a>
+            </div>
+          </template>
+
+          <template v-if="activeTab === 'past'">
+            <div class="past-events">
+              <event-card
+                v-for="event in displayedPastEvents"
+                :key="event.sys.id"
+                :event="event"
+              />
+            </div>
+
+            <div class="show-more-past-events">
+              <a
+                v-if="!isShowingAllPastEvents && pastEventsChunkMax > 1"
+                class="show-more-past-events__btn"
+                href="#"
+                @click.prevent="pastEventChunk += 1"
+              >
+                View More
+              </a>
+            </div>
+          </template>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { pathOr } from 'ramda'
+
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb.vue'
+import EventCard from '@/components/EventCard/EventCard'
+import PageHero from '@/components/PageHero/PageHero.vue'
+import TabNav from '@/components/TabNav/TabNav.vue'
+
+import FormatDate from '@/mixins/format-date'
+
+import createClient from '@/plugins/contentful.js'
+
+const client = createClient()
+const MAX_PAST_EVENTS = 8
+
+const getEvents = async () => {
+  try {
+    const todaysDate = new Date()
+
+    const upcomingEvents = await client.getEntries({
+      content_type: process.env.ctf_event_id,
+      order: 'fields.startDate',
+      'fields.startDate[gte]': todaysDate.toISOString()
+    })
+
+    const pastEvents = await client.getEntries({
+      content_type: process.env.ctf_event_id,
+      order: 'fields.startDate',
+      'fields.startDate[lt]': todaysDate.toISOString()
+    })
+
+    return {
+      upcomingEvents: upcomingEvents.items,
+      pastEvents: pastEvents.items
+    }
+  } catch {
+    return {
+      upcomingEvents: [],
+      pastEvents: []
+    }
+  }
+}
+
+export default {
+  name: 'EventsPage',
+
+  components: {
+    Breadcrumb,
+    EventCard,
+    PageHero,
+    TabNav
+  },
+
+  mixins: [FormatDate],
+
+  async asyncData() {
+    const events = await getEvents()
+
+    return {
+      ...events
+    }
+  },
+
+  data: function() {
+    return {
+      title: 'News & Events',
+      breadcrumb: [
+        {
+          to: {
+            name: 'index'
+          },
+          label: 'Home'
+        }
+      ],
+      activeTab: 'upcoming',
+      eventsTabs: [
+        {
+          label: 'Upcoming',
+          type: 'upcoming'
+        },
+        {
+          label: 'Past',
+          type: 'past'
+        }
+      ],
+      upcomingEvents: [],
+      pastEvents: [],
+      isShowingAllUpcomingEvents: false,
+      isShowingAllPastEvents: false,
+      pastEventChunk: 1
+    }
+  },
+
+  computed: {
+    /**
+     * Compute upcoming events
+     * Used to display four events in the upcoming tab
+     * @returns {Array}
+     */
+    displayedUpcomingEvents: function() {
+      return this.isShowingAllUpcomingEvents
+        ? this.upcomingEvents
+        : this.upcomingEvents.slice(0, 4)
+    },
+
+    /**
+     * Compute maximum chunk value
+     * @returns {Number}
+     */
+    pastEventsChunkMax: function() {
+      return Math.ceil(this.pastEvents.length / MAX_PAST_EVENTS)
+    },
+
+    /**
+     * Compute past events to display based on
+     * current chunk value
+     * @returns {Array}
+     */
+    displayedPastEvents: function() {
+      if (this.pastEventChunk === this.pastEventsChunkMax)
+        this.isShowingAllPastEvents = true
+      const endChunk = this.pastEventChunk * MAX_PAST_EVENTS
+      return this.pastEvents
+        .slice()
+        .reverse()
+        .slice(0, endChunk)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/_variables.scss';
+
+h2 {
+  font-size: 1.5rem;
+  line-height: 2.25rem;
+}
+
+h3 {
+  color: $navy;
+  font-size: 1.375rem;
+  line-height: 2rem;
+}
+.event-card {
+  margin-bottom: 2em;
+}
+
+.upcoming-events,
+.past-events {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 -1em;
+}
+.upcoming-event {
+  margin: 0 1em 2em;
+  @media (min-width: 48em) {
+    width: calc(50% - 4.125em); // Account for the margins and the border
+  }
+  @media (min-width: 64em) {
+    width: calc(25% - 4.125em); // Account for the margins and the border
+  }
+}
+.show-all-upcoming-events {
+  text-align: center;
+  &__btn {
+    align-items: center;
+    color: $navy;
+    display: inline-flex;
+    font-weight: 500;
+    text-decoration: none;
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+  }
+  .svg-icon {
+    margin-left: 0.5rem;
+  }
+}
+
+.show-more-past-events {
+  text-align: center;
+  &__btn {
+    display: inline-flex;
+    border: 1px solid $dark-gray;
+    border-radius: 5px;
+    text-decoration: none;
+    padding: 8px 10px;
+    font-size: 11pt;
+    font-weight: bold;
+    color: $dark-gray;
+  }
+}
+.events-wrap {
+  margin-bottom: 2.625em;
+}
+</style>
