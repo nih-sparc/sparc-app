8212eabd52cf75ec0716e943f3a67711bd87463e
Maha Zayed <maha@blackfynn.com>
2021-03-29 16:36:19 -0400

    working on pagination

--- pages/news-and-events/news/index.vue
+++ pages/news-and-events/news/index.vue
@@ -1,71 +1,103 @@
 <template>
-<div>
-<breadcrumb :breadcrumb="breadcrumb" />
+  <div>
+    <breadcrumb :breadcrumb="breadcrumb" />
     <page-hero>
       <h1>Latest News</h1>
       <p>
-        Check out the latest announcements about the SPARC portal and the latest news about the SPARC program.
+        Check out the latest announcements about the SPARC portal and the latest
+        news about the SPARC program.
       </p>
     </page-hero>
     <div class="page-wrap container">
       <div class="subpage">
-        <news-list-item v-for="item in this.items" :key="item.sys.id" :item="item" />
+        <news-list-item
+          v-for="item in this.items"
+          :key="item.sys.id"
+          :item="item"
+        />
+      </div>
+      <div class="metadata-table-pagination">
+        <pagination
+          :selected="this.page"
+          :page-size="this.limit"
+          :total-count="this.total"
+          @select-page="getMoreNews"
+        />
       </div>
     </div>
-</div>
-
+  </div>
 </template>
 
 <script lang="ts">
-import Breadcrumb from '@/components/Breadcrumb/Breadcrumb'
-import PageHero from '@/components/PageHero/PageHero'
-import NewsListItem from '@/components/NewsListItem/NewsListItem.vue'
+import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
+import PageHero from "@/components/PageHero/PageHero";
+import NewsListItem from "@/components/NewsListItem/NewsListItem.vue";
+import Pagination from "@/components/Pagination/Pagination.vue";
 
-import createClient from '@/plugins/contentful.js';
+import createClient from "@/plugins/contentful.js";
 
-import { fetchData, fetchNews, NewsCollection } from '../model';
+import { fetchNews, NewsCollection } from "../model";
 
-const client = createClient()
-const MAX_PAST_EVENTS = 8
+const client = createClient();
+const MAX_PAST_EVENTS = 8;
 
-    export default {
-        name: 'News',
-        components: {
-            Breadcrumb,
-            PageHero,
-            NewsListItem
+const getMoreNews = async () => {
+  try {
+    return await fetchNews(client, '', 10, 10)
+  } catch (error) {
+    console.log('hello!')
+  }
+}
+
+export default {
+  name: "News",
+  components: {
+    Breadcrumb,
+    PageHero,
+    NewsListItem,
+    Pagination,
+  },
+
+  asyncData() {
+    return fetchNews(client, "", 10);
+  },
+
+  data() {
+    return {
+      breadcrumb: [
+        {
+          label: "Home",
+          to: {
+            name: "index",
+          },
         },
-
-         asyncData() {
-            return fetchNews(client, '', 10)
+        {
+          label: "News & Events",
+          to: {
+            name: "news-and-events",
+          },
         },
-
-        data() {
-            return {
-              breadcrumb: [
-             {
-                label: 'Home',
-                 to: {
-                    name: 'index'
-                 }
-             },
-             {
-                label: 'News & Events',
-                to: {
-                  name: 'news-and-events'
-                }
-             },
-             {
-                label: 'News',
-                to: {
-                  name: 'news'
-                }
-             }
-            ],
-            news: {} as NewsCollection
-            }
+        {
+          label: "News",
+          to: {
+            name: "news",
+          },
         },
+      ],
+      news: {} as NewsCollection,
+      offset: 0
     }
+  },
+
+  methods: {
+     getMoreNews: async function() {
+        this.offset = this.offset + this.limit
+        const response = await fetchNews(client, "", 10, this.offset)
+        this.items = response.items
+    }
+  },
+
+}
 </script>
 
 <style lang="scss" scoped>
