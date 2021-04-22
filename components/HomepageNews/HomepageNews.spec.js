17316777ac61b4fbbd93985fc689f9c10e04b5ff
Cameron Baney <cbaney@gmail.com>
2021-04-22 17:24:23 -0400

    Add unit tests

--- components/HomepageNews/HomepageNews.spec.js
+++ components/HomepageNews/HomepageNews.spec.js
@@ -1,15 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import HomepageNews from './HomepageNews.vue'
-import news from './mockNews'
+import { events, internalEvent } from './mockNews'
 
 describe('HomepageNews.vue', () => {
   it('filters only future news or news without a date', () => {
     const wrapper = shallowMount(HomepageNews, {
       propsData: {
-        news
+        news: events
       },
       stubs: {
-        NuxtLink: true,
+        NuxtLink: RouterLinkStub,
         SvgIcon: true,
         ElButton: true
       }
@@ -17,5 +17,37 @@ describe('HomepageNews.vue', () => {
 
     expect(wrapper.vm.news.length).toBe(4)
     expect(wrapper.vm.upcomingNews.length).toBe(2)
+  })
+
+  it('Renders a NuxtLink if the entry has a details page', () => {
+    const wrapper = shallowMount(HomepageNews, {
+      propsData: {
+        news: internalEvent
+      },
+      stubs: {
+        NuxtLink: RouterLinkStub,
+        SvgIcon: true,
+        ElButton: true
+      }
+    })
+
+    const link = wrapper.findComponent(RouterLinkStub)
+    expect(link.exists()).toBe(true)
+  })
+
+  it('Renders an a tag if the entry does not have a details page', () => {
+    const wrapper = shallowMount(HomepageNews, {
+      propsData: {
+        news: events
+      },
+      stubs: {
+        NuxtLink: RouterLinkStub,
+        SvgIcon: true,
+        ElButton: true
+      }
+    })
+
+    const link = wrapper.find('h3 NuxtLink')
+    expect(link.exists()).toBe(false)
   })
 })
