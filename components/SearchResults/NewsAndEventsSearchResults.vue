03abc52e4e2db862169d49be2711a51fa4278a90
Eric Gauzens <ericgauzens@hotmail.com>
2021-10-05 16:39:06 -0400

    Added Community Spotlight to News Facet Menu

--- components/SearchResults/NewsAndEventsSearchResults.vue
+++ components/SearchResults/NewsAndEventsSearchResults.vue
@@ -4,11 +4,16 @@
       v-if="this.$route.query.newsAndEventsType === 'event'"
       :table-data="tableData"
     />
+    <community-spotlight-listings 
+      v-else-if="this.$route.query.newsAndEventsType === 'successStoryDisplay'" 
+      :stories="tableData" 
+    />
     <news-search-results v-else :table-data="tableData" />
   </div>
 </template>
 
 <script>
+import CommunitySpotlightListings from '../CommunitySpotlight/CommunitySpotlightListings.vue'
 import NewsSearchResults from './NewsSearchResults.vue'
 import EventSearchResults from './EventSearchResults.vue'
 
@@ -16,6 +21,7 @@ export default {
   name: 'NewsAndEventsSearchResults',
 
   components: {
+    CommunitySpotlightListings,
     NewsSearchResults,
     EventSearchResults
   },
