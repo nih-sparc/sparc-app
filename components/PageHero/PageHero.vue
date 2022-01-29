fbb7679668447a528485087fbfd9b31bfa5b6143
Eric Gauzens <ericgauzens@hotmail.com>
2022-01-29 16:10:53 -0500

    Added tags and fixed header

--- components/PageHero/PageHero.vue
+++ components/PageHero/PageHero.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="page-hero">
-    <div class="container">
-      <div class="row">
-        <div class="col page-hero__copy">
-          <slot />
-        </div>
-        <div v-if="$slots['image']" class="page-hero__image">
+<div class="page-hero">
+  <div class="bx--grid">
+    <div class="bx--row">
+      <div class="bx--col-sm-4 bx--col-md-8 bx--col-lg-10 bx--col-xlg-10 py-32 px-0 text-container">
+        <slot />
+      </div>
+      <div class="bx--col-sm-0 bx--col-md-0 bx--col-lg-6 bx--col-xlg-6">
+        <div v-if="$slots['image']" class="graphic-container">
           <slot name="image" />
         </div>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -25,65 +27,50 @@ export default {
 }
 </script>
 
-<style lang="scss">
-@import '../../assets/_variables.scss';
+<style lang="scss" scoped>
+@import '@nih-sparc/sparc-design-system-components/src/assets/_variables.scss';
 
 .page-hero {
-  background: $navy;
+  background: $darkBlue;
   color: #f0f2f5;
   font-size: 1em;
   line-height: 1.5rem;
-  padding: 2rem 1rem;
-  @media (min-width: 48em) {
-    font-size: 1.25em;
-    line-height: 2rem;
-    padding: 2.5rem 0;
-  }
-  &__copy {
-    position: relative;
-    z-index: 1;
-    @media (min-width: 64em) {
-      max-width: 53rem;
-    }
-    a {
-      color: #fff;
-      text-decoration: underline;
-    }
-  }
-  &__image {
-    display: none;
-
-    @media (min-width: 48em) {
-      display: flex;
-      height: calc(100% + 5rem);
-      justify-content: flex-end;
-      right: 0;
-      position: absolute;
-      top: -2.5rem;
-      width: auto;
-    }
-    img,
-    video {
-      display: block;
-      height: 100%;
-      width: inherit;
-    }
-  }
-  .container {
-    position: relative;
-  }
   h1 {
     font-size: 1.75em;
     margin-bottom: 0.5rem;
     line-height: 1.2;
   }
-  h1,
-  p {
-    background-color: rgba(36, 36, 91, 0.75);
+  .bx--grid {
+    max-width: 1280px;
+    padding: 0;
+    .bx--row {
+      align-items: center;
+      margin: 0;
+    }
+    .bx--col {
+      padding: 0;
+    }
   }
-
-  .row {
+  @media (min-width: 80em) {
+    font-size: 1.25em;
+    line-height: 2rem;
+  }
+  .graphic-container {
     display: flex;
+    img, video {
+      height: 100%;
+      width: stretch;
+    }
+  }
+  .text-container {
+    @media (max-width: 80em) {
+      padding-left: 1em;
+      padding-right: 1em;
+    }
+    ::v-deep a {
+      color: white;
+      text-decoration: underline;
+    }
   }
 }
 </style>
