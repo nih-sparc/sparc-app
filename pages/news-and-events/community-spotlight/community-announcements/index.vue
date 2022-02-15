9cdefba651bdadfd4dd5c6784d760e5f8af2a247
Jesse Khorasanee <jessekhorasanee@gmail.com>
2022-02-15 19:46:58 +1300

    Fix breadcrumb having wrong title

--- pages/news-and-events/community-spotlight/community-announcements/index.vue
+++ pages/news-and-events/community-spotlight/community-announcements/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="help-page">
-    <breadcrumb :breadcrumb="breadcrumb" title="Fireside Chats" />
+    <breadcrumb :breadcrumb="breadcrumb" title="Community Spotlight" />
     <page-hero>
       <h1>Community Announcements</h1>
       <br />
