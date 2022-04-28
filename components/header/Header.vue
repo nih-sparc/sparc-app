bf4b9e8a9bff64898197852b11a500dbac8ed194
Eric Gauzens <ericgauzens@hotmail.com>
2022-04-28 14:02:15 -0400

    check for string

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -10,7 +10,7 @@
         <nuxt-link :to="{ name: 'help' }">
           Help
         </nuxt-link>
-        <template v-if="showLoginFeature">
+        <template v-if="showLoginFeature === 'false'">
           <img
             class="mr-2"
             src="@/static/images/orcid_24x24.png"
