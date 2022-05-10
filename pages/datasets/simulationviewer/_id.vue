9b6017b28052ac624c6c559070cef87c2b32df85
Alan Garny <agarny@hellix.com>
2022-05-10 16:31:23 +1200

    Simulation viewer page.

---
+++ pages/datasets/simulationviewer/_id.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="simulation-viewer-page">
+    <div class="page-wrap container">
+      <detail-tabs :active-tab="activeTab" :tabs="tabs" @set-active-tab="activeTab = $event">
+        <client-only placeholder="Loading simulation ...">
+          <div class="simulation-vuer-container">
+            <SimulationVuer :id="id" />
+          </div>
+        </client-only>
+      </detail-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+import DetailTabs from "@/components/DetailTabs/DetailTabs.vue";
+
+export default {
+  name: "SimulationViewerPage",
+  components: {
+    DetailTabs,
+    SimulationVuer: process.client
+      ? () => import("@abi-software/simulationvuer").then(m => m.SimulationVuer)
+      : null,
+  },
+  data: function () {
+    return {
+      activeTab: "simulation",
+      apiLocation: process.env.portal_api,
+      id: Number(this.$route.query.id),
+      tabs: [
+        {
+          label: "Simulation Viewer",
+          type: "simulation",
+        }
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.simulation-vuer-container {
+  @import '~@abi-software/simulationvuer/dist/simulationvuer';
+  min-height: 24px;
+}
+</style>
