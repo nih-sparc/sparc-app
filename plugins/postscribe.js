824c0a9f02aafc347d6790c363b6adb7a2176e83
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2022-06-07 19:52:27 +0200

    added newsletter archive

---
+++ plugins/postscribe.js
@@ -0,0 +1,10 @@
+import postscribe from 'postscribe'
+
+export default (_, inject) => {
+  inject('injectNewsletterArchive', elementSelector => {
+    postscribe(
+      elementSelector,
+      '<script language="javascript" src="//science.us2.list-manage.com/generate-js/?u=e60c48f231a30b544eed731ea&fid=46414&show=10" type="text/javascript"></script>'
+    )
+  })
+}
