7e287608c1b516321e1b392bd0146019e060da75
alan-wu <alan.wu@auckland.ac.nz>
2023-10-19 11:04:49 +1300

    Update github actions.

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   defaultCommandTimeout: 5000,
   e2e: {
     baseUrl: process.env.ROOT_URL ? process.env.ROOT_URL : 'http://localhost:3000',
-    projectId: "iobp4a",
+    projectId: process.env.CYPRESS_PROJECT_ID,
     viewportWidth: 1600,
     viewportHeight: 900,
     // pageLoadTimeout: 1024*1024*1024,
