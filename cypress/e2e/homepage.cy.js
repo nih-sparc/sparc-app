1b897ca4a87679793bd1884ed20feacf1d03aaa4
alan-wu <alan.wu@auckland.ac.nz>
2023-10-19 11:06:29 +1300

    Add a new sample test.

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -3,4 +3,8 @@ describe('Homepage', () => {
     cy.visit('')
     cy.get('h1').should('contain', 'Welcome to the SPARC Portal')
   })
+  it('Navigation Bar', () => {
+    cy.visit('')
+    cy.get('.mobile-navigation > :nth-child(1) > :nth-child(1) > a').should('contain', 'Data')
+  })
 })
