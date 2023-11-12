1a7b021ce0bdc6eae589c9dbb39be8bb7bd23eb3
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-11-13 09:58:21 +1300

    add keyword search test

--- cypress/e2e/finddata.cy.js
+++ cypress/e2e/finddata.cy.js
@@ -1,9 +1,44 @@
-describe('Find Data page', () => {
-  it('Go to dataset', () => {
-    cy.visit('')
-    cy.get('.mobile-navigation > :nth-child(1) > :nth-child(1) > a').click()
-    cy.url().should('include', '/data?type=dataset')
-    cy.get(':nth-child(1) > .el-table_1_column_2 > .cell > :nth-child(1) > a').click()
-    cy.url().should('include', '/datasets/')
+const categories = ["dataset"]
+// const categories = ["dataset", "model", "simulation", "projects"]
+
+const keywords = ["Spine", "neck"]
+
+categories.forEach(category => {
+
+  describe(`Find Data in ${category}`, { testIsolation: false }, function () {
+    before(function () {
+      cy.visit(`/data?type=${category}`);
+    });
+
+    const searchInput = keywords.join(',')
+    it(`Keyword Search - ${searchInput}`, function () {
+      cy.get('.el-input__inner').type(searchInput);
+      cy.get('.btn-clear-search > .svg-icon').should('be.visible')
+      cy.get('.search-text').click();
+      cy.url({ decode: true }).should('include', searchInput)
+
+      cy.intercept('POST', '**/query?**').as('keywordSearchResult')
+      cy.wait('@keywordSearchResult');
+
+      cy.get(':nth-child(1) > p').then($p => {
+        const noResult = $p.text().includes('\n                  0 Results | Showing')
+        if (noResult) {
+          this.skip()
+        } else {
+          const regex = new RegExp(`${keywords.join('|')}`, 'i')
+          cy.get('.table-wrap').within(() => {
+            cy.get('b').contains(regex)
+          })
+        }
+      })
+      cy.get('.btn-clear-search > .svg-icon').click()
+      cy.get('.el-input__inner').should('have.attr', 'placeholder', 'Enter search criteria');
+    })
+
+    /*
+    There are situations that dataset cards do not show the (highlighted) keywords
+    Just in case this happens for all the displayed dataset cards, extra tests may need to be added
+    cy.get(':nth-child(1) > .el-table_1_column_2 > .cell').contains(regex);
+     */
   })
 })
