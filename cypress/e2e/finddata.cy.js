b7032310e8b93a8f5e858c772f302e1a1c7ce638
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-11-14 16:06:18 +1300

    Add multiple facets test

--- cypress/e2e/finddata.cy.js
+++ cypress/e2e/finddata.cy.js
@@ -5,6 +5,8 @@ const keywords = ["Spine", "neck"]
 
 const singleFacet = "Human"
 
+const multipleFacets = ["Human", "Organ"]
+
 categories.forEach(category => {
 
   describe(`Find Data in ${category}`, { testIsolation: false }, function () {
@@ -14,11 +16,8 @@ categories.forEach(category => {
 
     it('All Page Features', function () {
       cy.get('.label1 > .el-dropdown > .filter-dropdown').then(($dropdown) => {
-        // const defaultOrder = $dropdown.text().includes('Published (desc)')
-        // if (defaultOrder) {
         cy.wrap($dropdown).click()
         cy.contains('Z-A').click()
-        // }
         cy.wrap($dropdown).should('contain', 'Z-A')
       })
       cy.get('[role="tooltip"]').should('not.be.visible')
@@ -26,8 +25,7 @@ categories.forEach(category => {
       cy.get('[role="tooltip"]').should('be.visible')
       cy.get('[role="tooltip"]').should('contain', 'How do filters work?')
       cy.get(':nth-child(1) > p > .el-dropdown > .filter-dropdown').click()
-      // Two dropdown-menu exist in the page, can not locate the specific element, force click is require
-      cy.get('.el-dropdown-menu > .el-dropdown-menu__item').contains('20').click({ force: true })
+      cy.get('.el-dropdown-menu > .el-dropdown-menu__item:visible').contains('20').click()
       cy.get('.el-table__row').should('have.length', 20)
     })
     const searchInput = keywords.join(',')
@@ -47,7 +45,7 @@ categories.forEach(category => {
           cy.get('.el-table__empty-text').should('exist')
         } else {
           cy.get('.el-table__empty-text').should('not.exist')
-          const regex = new RegExp(`${keywords.join('|')}`, 'i')
+          const regex = new RegExp(keywords.join('|'), 'i')
           cy.get('.table-wrap').within(() => {
             cy.get('b').contains(regex)
           })
@@ -58,15 +56,15 @@ categories.forEach(category => {
       // // Just in case this happens for all the displayed dataset cards, extra tests may need to be added
       // cy.get(':nth-child(1) > .el-table_1_column_2 > .cell').contains(regex)
     })
-    it(`Faceted Browse Search - ${singleFacet}`, function () {
+    it(`Single Faceted Browse Search - ${singleFacet}`, function () {
       // let facetCategory
       cy.get('.no-facets').should('contain', 'No filters applied')
       cy.get('.label-content-container').should('not.be.visible')
       cy.get('.expand-all-container > .el-link > .el-link--inner').click()
       cy.get('.label-content-container').should('be.visible')
       cy.get('.label-content-container').then(($label) => {
-        const facetExistInFilter = $label.find('span.capitalize').text().toLocaleLowerCase().includes(singleFacet.toLocaleLowerCase())
-        if (facetExistInFilter) {
+        const singleFacetExist = $label.find('span.capitalize:visible').text().toLowerCase().includes(singleFacet.toLowerCase())
+        if (singleFacetExist) {
           // cy.wrap($label).contains(singleFacet).parents('.sparc-design-system-component-dropdown-multiselect').within(() => {
           //   cy.get('.label-title').then(($el) => {
           //     facetCategory = $el.text()
@@ -74,11 +72,9 @@ categories.forEach(category => {
           // })
           const regex = new RegExp(singleFacet, 'i')
           cy.wrap($label).contains(regex).click()
-          cy.url({ decode: true }).should('contain', 'selectedFacetIds')
           cy.get('.el-tag__close').eq(0).click()
           cy.get('.no-facets').should('contain', 'No filters applied')
           cy.wrap($label).contains(regex).click()
-          cy.url({ decode: true }).should('contain', 'selectedFacetIds')
           cy.get('.tags-container > .flex > .el-link > .el-link--inner').click()
           // // "No filters applied" will not appear after click "reset all" in Cypress. BUG?
           // cy.get('.no-facets').should('contain', 'No filters applied')
@@ -108,6 +104,46 @@ categories.forEach(category => {
           this.skip()
         }
       })
+      cy.get('.tags-container > .flex > .el-link > .el-link--inner').click()
+    })
+    it(`Multiple Faceted Browse Search - ${multipleFacets}`, function () {
+      cy.get('.expand-all-container > .el-link > .el-link--inner').click()
+      cy.get('.label-content-container').then(($label) => {
+        let multipleFacetsExist = true
+        multipleFacets.forEach((facet) => {
+          multipleFacetsExist = multipleFacetsExist && $label.find('span.capitalize:visible').text().toLowerCase().includes(facet.toLowerCase())
+        })
+        if (multipleFacetsExist) {
+          multipleFacets.forEach((facet) => {
+            const singleRegex = new RegExp(`^${facet}$`, 'i')
+            cy.wrap($label).contains(singleRegex).click()
+          })
+          const multipleRegex = new RegExp(multipleFacets.join('|'), 'i')
+          cy.get('.el-card__body > .capitalize').contains(multipleRegex)
+          cy.get(':nth-child(1) > p').then(($p) => {
+            const noResult = $p.text().includes('\n                  0 Results | Showing')
+            if (noResult) {
+              cy.get('.el-table__empty-text').should('exist')
+            } else {
+              cy.get('.el-table__empty-text').should('not.exist')
+              cy.get('.table-wrap').then(($content) => {
+                let facetsExistInCard = true
+                multipleFacets.forEach((facet) => {
+                  facetsExistInCard = facetsExistInCard || $content.text().toLowerCase().includes(facet.toLowerCase())
+                })
+                if (facetsExistInCard) {
+                  cy.wrap($content).contains(multipleRegex)
+                } else {
+                  this.skip()
+                }
+              })
+            }
+          })
+        } else {
+          this.skip()
+        }
+      })
+      cy.get('.tags-container > .flex > .el-link > .el-link--inner').click()
     })
   })
 })
