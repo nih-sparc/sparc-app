b1766df66571c5e850471ec27158c95dcb0db8ad
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-11-16 16:15:06 +1300

    base testing for scaffold/flatmap

---
+++ test/cypress/e2e/mapviewer.cy.js
@@ -0,0 +1,60 @@
+const datasetIds = [150, 155]
+
+const apinatomyModels = ['Rat', 'Human Female']
+
+describe('Maps viewer', { testIsolation: false }, function () {
+  before(function () {
+    cy.visit('/maps?type=ac')
+  })
+
+  apinatomyModels.forEach((model) => {
+    it.only(`Provenance card for ${model}`, function () {
+      cy.wait(5000)
+      cy.get('#flatmap-select').click()
+      cy.get('.el-select-dropdown__item').should('be.visible')
+      cy.get('.el-select-dropdown__item:visible').filter(`:contains(${model})`).click()
+      cy.get('.maplibregl-touch-drag-pan > .maplibregl-canvas').click({ multiple: true });
+      cy.get('.maplibregl-popup-content').should('be.visible')
+      cy.get('.maplibregl-popup-content').within(() => {
+        cy.get('.title').should('exist')
+        cy.get('.subtitle').should('exist')
+        cy.contains(/COMPONENTS/i).should('exist')
+      })
+      cy.get('.maplibregl-popup-close-button').click()
+    })
+  })
+  it('From 2D human, open 3D map for synchronised view (split screen)', function () {
+    cy.get('.open-tab > .el-icon-arrow-left').click();
+    cy.get('.close-tab > .el-icon-arrow-right').click();
+  })
+  it('Search within display', function () {
+    cy.get('.open-tab > .el-icon-arrow-left').click();
+    cy.get('.close-tab > .el-icon-arrow-right').click();
+  })
+
+  datasetIds.forEach((datasetId) => {
+
+    it(`Context card in sidebar for scaffold dataset ${datasetId}`, function () {
+      cy.get('.open-tab > .el-icon-arrow-left').click()
+      cy.get('.search-input > .el-input__inner').clear()
+      cy.get('.search-input > .el-input__inner').type(datasetId)
+      cy.get('.header > .el-button > span').click()
+      cy.wait(500)
+      cy.get('.dataset-results-feedback').then(($result) => {
+        const noResult = $result.text() === '0 results | Showing'
+        if (noResult) {
+          cy.get('.error-feedback').should('exist')
+          cy.get('.error-feedback').should('contain', 'No results found')
+        } else {
+          cy.get('.dataset-card').contains(datasetId).should('exist')
+          cy.get('.dataset-card').filter(`:contains(${datasetId})`).within(() => {
+            cy.wait(5000)
+            cy.get('.container').contains(/Scaffold/i).click()
+          })
+        }
+      })
+      cy.contains(/View Scaffold/i).click()
+      cy.get('.close-tab > .el-icon-arrow-right').click();
+    })
+  })
+})
