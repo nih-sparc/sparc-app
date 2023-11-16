257d80000df61660951896433ee79ce5f31e7f63
Eric Gauzens <ericgauzens@hotmail.com>
2023-11-16 12:51:39 -0500

    testing datalayer push

--- pages/datasets/_datasetId.vue
+++ pages/datasets/_datasetId.vue
@@ -369,14 +369,9 @@ export default {
 
   mounted() {
     this.$gtm.push({
-      event: 'interaction_event',
-      event_name: 'dataset_page_view',
       dataset_id: propOr(this.$route.params.datasetId, 'id', this.datasetInfo),
       version_id: propOr('undefined', 'version', this.datasetInfo),
       doi: propOr('undefined', 'doi', this.datasetInfo)
-    })
-    this.$nextTick(() => {
-      window['google_tag_manager'][`${process.env.GOOGLE_TAG_MANAGER_ID}`].dataLayer.reset()
     })
   },
 
