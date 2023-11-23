99e067c92bec225d1234af1995dcab1f046fc3cc
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-11-23 14:01:33 +1300

    update navbar testing

--- test/cypress/e2e/homepage.cy.js
+++ test/cypress/e2e/homepage.cy.js
@@ -1,11 +1,17 @@
-describe('Homepage', () => {
+describe('Homepage', { testIsolation: false }, function () {
   beforeEach(function () {
     cy.visit('')
   });
-  it('Banner', () => {
-    cy.get('h1').should('contain', 'SPARC')
+  it('Banner', function () {
+    cy.get('h1').contains(/SPARC/).should('exist')
+    cy.get('.btn-link > .el-button > span').contains(/Submit to SPARC/i).should('exist')
   })
-  it('Navigation Bar', () => {
-    cy.get('.mobile-navigation > :nth-child(1) > :nth-child(1) > a').should('contain', 'Data')
+  it('Navigation Bar', function () {
+    cy.get('.mobile-navigation > :nth-child(1) > :nth-child(1) > a').contains(/Data & Models/i).should('exist')
+    cy.get('.mobile-navigation > :nth-child(1) > :nth-child(2) > a').contains(/Tools & Resources/i).should('exist')
+    cy.get('.mobile-navigation > :nth-child(1) > :nth-child(3) > a').contains(/Maps/i).should('exist')
+    cy.get('.mobile-navigation > :nth-child(1) > :nth-child(4) > a').contains(/News & Events/i).should('exist')
+    cy.get(':nth-child(1) > :nth-child(5) > a').contains(/About/i).should('exist')
+    cy.get(':nth-child(1) > :nth-child(6) > a').contains(/Submit to SPARC/i).should('exist')
   })
 })
