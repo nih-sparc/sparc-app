ef5c7a8f665b2d0c67cbf77502bfdac65bedbf0b
ddjnw1yu <david.jianwei.yu@outlook.com>
2023-12-05 10:22:47 +1300

    extend view scaffold use flow
    redirect back to dataset from map viewer

--- test/cypress/e2e/userstories.cy.js
+++ test/cypress/e2e/userstories.cy.js
@@ -1,20 +1,23 @@
 // The position of selected datasets
-const searchInput = 'segmentation'
+const segmentationUseFlowSearchInput = 'segmentation'
+const scaffoldUseFlowSearchInput = 'scaffold'
 const datasetIndex = [1, 2]
 
 describe('User stories', function () {
-  describe('Should find segmentation data in the gallery', { testIsolation: false }, function () {
-    it(`Search for ${searchInput}`, function () {
+  describe('Should find segmentation in the gallery', { testIsolation: false }, function () {
+    before(`Search for ${segmentationUseFlowSearchInput}`, function () {
       cy.intercept('**/query?**').as('query');
       cy.visit('');
+      cy.wait('@query', { timeout: 20000 });
 
       // Navigate to 'Data&Models' page
-      cy.get('.mobile-navigation > :nth-child(1) > :nth-child(1) > a').click();
+      cy.get('.mobile-navigation > :nth-child(1) > :nth-child(1) > a', { timeout: 30000 }).click();
+      cy.wait(500)
 
       // Search for segmentation related dataset
-      cy.get('.el-input__inner').type(searchInput);
+      cy.get('.el-input__inner').type(segmentationUseFlowSearchInput);
       cy.get('.search-text').click();
-      cy.wait('@query', { timeout: 20000 });
+      cy.wait(500)
     })
 
     datasetIndex.forEach((index) => {
@@ -38,15 +41,24 @@ describe('User stories', function () {
     })
   })
   describe('Should find scaffold in the gallery', { testIsolation: false }, function () {
-    it('Go to Anatomical Models', function () {
+    before('Go to Anatomical Models', function () {
+      cy.intercept('**/query?**').as('query');
       cy.visit('');
+      cy.wait('@query', { timeout: 20000 });
+
       // Navigate to 'Data&Models' page
       cy.get('.mobile-navigation > :nth-child(1) > :nth-child(1) > a').click();
+      cy.wait(500)
 
       // Go to 'Anatomical Models'
       cy.get(':nth-child(2) > .search-tabs__button').click();
-      cy.wait(1500)
       cy.get(':nth-child(2) > .search-tabs__button').should('have.class', 'active');
+      cy.wait(500)
+
+      // Search for scaffold related dataset
+      cy.get('.el-input__inner').type(scaffoldUseFlowSearchInput);
+      cy.get('.search-text').click();
+      cy.wait(500)
     })
 
     datasetIndex.forEach((index) => {
@@ -54,7 +66,7 @@ describe('User stories', function () {
         cy.get('.el-table__row', { timeout: 30000 }).eq(index - 1).as('dataset');
 
         // Enter the dataset
-        cy.get('@dataset').find('.cell > :nth-child(1) > a').last().click();
+        cy.get('@dataset').find('.cell > :nth-child(1) > a', { timeout: 30000 }).last().click();
 
         // Enter the 'Gallery' tab
         cy.get(':nth-child(5) > .style1').click();
@@ -67,42 +79,49 @@ describe('User stories', function () {
 
         // cy.get('@scaffold').first().click()
         // Alternative solution
-
         cy.url().then(($url) => {
           const datasetId = $url.substring($url.lastIndexOf("/") + 1, $url.indexOf("?"));
           cy.get('.dataset-information-box > :nth-child(1)').then(($version) => {
-            cy.intercept('**/query?**').as('query')
-            cy.intercept('**/dataset_info/**').as('dataset_info')
-            cy.intercept('**/datasets/**').as('datasets')
+            cy.intercept('**/query?**').as('query');
+            cy.intercept('**/dataset_info/**').as('dataset_info');
+            cy.intercept('**/datasets/**').as('datasets');
 
             const version = $version.text().match(/[0-9]+/);
             cy.visit(`/maps?type=scaffold&dataset_id=${datasetId}&dataset_version=${version}`);
 
-            cy.wait('@query', { timeout: 20000 })
+            cy.wait('@query', { timeout: 20000 });
 
             // Search dataset id
-            cy.get('.search-input > .el-input__inner').clear()
+            cy.get('.search-input > .el-input__inner').clear();
             cy.get('.search-input > .el-input__inner').type(datasetId);
             cy.get('.header > .el-button').click();
 
-            cy.wait(['@dataset_info', '@datasets'], { timeout: 20000 })
+            cy.wait(['@dataset_info', '@datasets'], { timeout: 20000 });
 
             // Check for search result and the tag 'Scaffold'
-            cy.get('.dataset-card-container > .dataset-card', { timeout: 30000 }).contains(datasetId).should('exist')
-            cy.get('.dataset-card-container > .dataset-card:visible').filter(`:contains(${datasetId})`).within(() => {
-              cy.get('.badges-container > .container', { timeout: 30000 }).contains(/Scaffold/i).click()
-            })
+            cy.get('.dataset-card-container > .dataset-card:visible', { timeout: 30000 }).as('datasetCards');
 
-            // Check for button text
-            cy.get('.dataset-card-container > .dataset-card:visible', { timeout: 30000 }).contains(/View Scaffold/i).click()
+            cy.get('@datasetCards').filter(`:contains(${datasetId})`).within(() => {
+              cy.get('.badges-container > .container', { timeout: 30000 }).contains(/Scaffold/i).click();
+            });
+
+            cy.get('@datasetCards').contains(/View Scaffold/i).click();
 
             // Loading mask should not exist after the scaffold is loaded
-            cy.get('.multi-container > .el-loading-parent--relative > .el-loading-mask', { timeout: 30000 }).should('not.exist')
+            cy.get('.multi-container > .el-loading-parent--relative > .el-loading-mask', { timeout: 30000 }).should('not.exist');
 
             // Check for context card
-            cy.get('.context-card').should('be.visible')
-            cy.get('.context-image').should('have.attr', 'src').and('contain', datasetId)
-            cy.get('[style="margin-right: 8px;"] > .title').should('have.class', 'title')
+            cy.get('.context-card').should('be.visible');
+            cy.get('.context-image').should('have.attr', 'src').and('contain', datasetId);
+            cy.get('[style="margin-right: 8px;"] > .title').should('have.class', 'title');
+
+            cy.get('.open-tab > .el-icon-arrow-left').click();
+
+            cy.get('@datasetCards').contains(/All/i).click();
+
+            // cy.get('@datasetCards').contains(/View Dataset/i).click();
+            // Alternative solution
+            cy.visit(`/datasets/${datasetId}?type=dataset`);
           })
         })
         cy.goBackToBrowser('model');
